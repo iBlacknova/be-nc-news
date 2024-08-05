87a8cc28b31cd48df48eb111b2710e0f2f4627d3
Blacknova7 <87141874+iBlacknova@users.noreply.github.com>
2024-08-05 13:57:29 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,59 +1,69 @@
-const express = require('express');
-const {endpoints} = require('./endpoints.json');
-const {getTopics,getAllEndpoints, getArticlesById, getAllArticles, getAllComments, postComment, patchArticleById, removeCommentById} = require('./controller');
+const express = require("express");
+const { endpoints } = require("./endpoints.json");
+const {
+  getTopics,
+  getAllEndpoints,
+  getArticlesById,
+  getAllArticles,
+  getAllComments,
+  postComment,
+  patchArticleById,
+  removeCommentById,
+} = require("./controller");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
-app.get('/api/topics', getTopics);
+app.get("/api/topics", getTopics);
 
-app.get('/api', getAllEndpoints);
+app.get("/api", getAllEndpoints);
 
-app.get('/api/articles/:article_id', getArticlesById);
+app.get("/api/articles/:article_id", getArticlesById);
 
-app.get('/api/articles', getAllArticles);
+app.get("/api/articles", getAllArticles);
 
-app.get('/api/articles/:article_id/comments', getAllComments);
+app.get("/api/articles/:article_id/comments", getAllComments);
 
-app.post('/api/articles/:article_id/comments', postComment);
+app.post("/api/articles/:article_id/comments", postComment);
 
-app.patch('/api/articles/:article_id', patchArticleById);
+app.patch("/api/articles/:article_id", patchArticleById);
 
-app.delete('/api/comments/:comment_id', removeCommentById);
-
-app.use((err, req, res, next) => {
-    if (err.code === '22P02'){
-        res.status(400).send({msg: '400 Bad request'});
-    }
-    if (err.code === '23503'){
-        res.status(404).send({msg: 'Username is required'});
-    }
-    if (err.code === '23502'){
-        res.status(400).send({msg: 'Body is required'});
-    }
-    else {
-        next(err);
-    }
-});
-
+app.delete("/api/comments/:comment_id", removeCommentById);
 
 app.use((err, req, res, next) => {
-    if (err.status && err.msg){
-        res.status(err.status).send({msg: err.msg})
-    }else {
-        next(err);
-    }
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "400 Bad request" });
+  }
+  if (err.code === "23503") {
+    res.status(404).send({ msg: "Username is required" });
+  }
+  if (err.code === "23502") {
+    res.status(400).send({ msg: "Body is required" });
+  } else {
+    next(err);
+  }
 });
 
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
 
-app.all('*', (req, res, next) => {
-     res.status(404).send({msg: '404 - request not found'})
+app.all("*", (req, res, next) => {
+  res.status(404).send({ msg: "404 - request not found" });
 });
 
 handleServerErrors = (err, req, res, next) => {
-    console.log(err);
-    res.status(500).send({msg: "Internal Server Error"})
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
 };
 
 app.use(handleServerErrors);
 
-module.exports = app
+module.exports = app;
